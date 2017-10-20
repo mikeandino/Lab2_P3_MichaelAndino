fe88c1678d58fb5f2af00c96609075f5bb0714ce
Michael Andino <mikeandinor98@gmail.com>
2017-10-20 13:29:47 -0600

    Creacion de metodo Menu

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
-#include  <iostream>;
+#include  <iostream>
 
 using namespace std;
 
 void menu();
 
-int 
+int  primos();
+
+int multiplos();
+
+int goldbach();
 
 int main(){
 
+}
+
+void menu(){
+	cout <<"Laboratorio 2 Programacion 3"<<endl
+		 <<"	1. Numeros Primos."<<endl
+		 <<"	2. Numeros Multiplos."<<endl
+		 <<" 	3. Conjetura Goldbach."<<endl;
 }
