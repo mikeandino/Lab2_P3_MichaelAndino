291d018d34e9b4a684aa153e305056ba9448a5a7
Michael Andino <mikeandinor98@gmail.com>
2017-10-20 13:54:57 -0600

    Termine Menu

--- main.cpp
+++ main.cpp
@@ -4,19 +4,58 @@ using namespace std;
 
 void menu();
 
-int  primos();
+void primos();
 
-int multiplos();
+int multiplos(int,int,int);
 
-int goldbach();
+int goldbach(int);
+
+bool primo(int);
 
 int main(){
 
 }
 
 void menu(){
-	cout <<"Laboratorio 2 Programacion 3"<<endl
-		 <<"	1. Numeros Primos."<<endl
-		 <<"	2. Numeros Multiplos."<<endl
-		 <<" 	3. Conjetura Goldbach."<<endl;
+	do {
+		cout <<"Laboratorio 2 Programacion 3"<<endl
+			 <<"	1. Numeros Primos."<<endl
+		 	<<" 	2. Numeros Multiplos."<<endl
+		 	<<" 	3. Conjetura Goldbach."<<endl;
+		int opcion;
+		cin >> opcion;
+		switch (opcion) {
+			case 1:{
+					primos();
+				}			
+				break;
+			case 2:{
+					int a,b,n;
+					cout<<"Ingrese el primer valor";
+					cin>>a;
+					cout<<endl<<"Ingrese el segundo valor: ";
+					cin>>b;
+					cout<<endl<<"Ingrese el tercer valor: ";
+					cin>>n;
+					cout<<endl<<"La suma de los numeros que son multiplos son: "+multiplos(a,b,n)<<endl;
+				}
+				break;
+			case 3:{
+					int num;
+					cout<<"Ingresa un numero menor que 150: ";
+					cin>>num;
+					while(num>150||num<1) {
+						cout<endl<<"Numero Invalido: ";
+						cin>>num;
+					}
+					goldbach(num);
+				}
+				break;
+			case 4:
+				cout<<"Por favor denme buena nota. :)"<<endl;
+				break;
+			default:
+				cout<<"Numero invalido"<<endl;
+		}
+	} while (opcion!=4);
 }
